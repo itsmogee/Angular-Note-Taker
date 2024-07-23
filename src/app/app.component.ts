0a32bfb80069cca5784e70a3077c8bbbf2ca23bc
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-23 16:37:02 +0200

    linked : header module to app-root
    header linked and used in the main component as the header.
    margin applied to the app-component to seperate it from the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostCreateComponent],
+  imports: [RouterOutlet, PostCreateComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mean-course';
