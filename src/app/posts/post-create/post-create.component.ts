7a3445a1e170b79744d7e183b07108b8f916fffa
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-23 15:20:39 +0200

    added : new component post-create
    basic boilerplate code for now

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-create',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css',
+})
+export class PostCreateComponent {
+  newPost = 'No Content';
+  enteredValue = '';
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
