0a1cc218f4e06021f847e2f89f1585075a7dcc42
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-23 16:52:22 +0200

    added  : UI styling & linked postlist component to app-component
    moved the width of the entire element to app-component instead of mat-card
    linked post-list to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostCreateComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
