fc5d0070c33b1c8be570c7395c9f6d71164af59a
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-24 00:33:06 +0200

    added : post backend endpoint
    provide alert on post request to verify functionality

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -34,7 +34,12 @@ export class PostsService {
       title: title,
       content: content,
     };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
