894747cadf3c9c6deb7d7b9a3e0ede21ade1ccde
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-24 13:58:43 +0200

    added : mongoose connections and Schemas
    Post schema defined and mongoose model created and exported

---
+++ backend/models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+}); // Blueprint
+
+// Mongoose needs a model to work with
+module.exports = mongoose.model("Post", postSchema);
