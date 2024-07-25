c61013c6975a257ff853be85b1ec3cebe4b1d1ea
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-25 12:08:19 +0200

    update : syling of the routes in the header
    created spacer between the two links in the header
    when the route is active styling is applied to indicate that
    removed list decorations etc

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  styleUrl: './header.component.css',
   templateUrl: './header.component.html',
-  imports: [MatToolbarModule, RouterLink],
+  imports: [MatToolbarModule, RouterLink, RouterLinkActive, MatButtonModule],
 })
 export class HeaderComponent {}
