581a444e4aae572ca754ee03d0de1dfac571095e
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-25 14:48:22 +0200

    refactor : backend routes
    created routes folder where different routes are handled
    posts.js handles anything dealing with /api/posts

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 
+const postsRoutes = require("./routes/posts");
+
 const app = express();
 
 mongoose
@@ -15,8 +17,6 @@ mongoose
   .catch(() => {
     console.log("Connection failed");
   });
-
-const Post = require("./models/post");
 
 app.use(bodyParser.json());
 
@@ -32,54 +32,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "Post added succesfully",
-      postID: createdPost._id,
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    res.status(200).json({ message: "update successful", post: post });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).status(200).json({
-      message: "Posts fetched succesfully!",
-      posts: documents,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found" });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    res.status(200).json({ message: "Post deleted" });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
