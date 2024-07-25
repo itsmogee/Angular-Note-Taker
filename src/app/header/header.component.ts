22a4d4064e93ce73aeb6a9c3093998b10b11ee91
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-25 11:34:47 +0200

    added : routes to the post create and post list components
    routes added to the header that link to the components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink],
 })
 export class HeaderComponent {}
