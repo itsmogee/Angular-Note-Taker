20e527938049dc3e1f2b383204be77d9b673f8a7
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-26 15:40:14 +0200

    added : image picker validator
    using mimetype, image validation is performed when picking the file using the filepicker
    if image is not correct type preview doesnt appear and upload can not happen

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -50,7 +51,10 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)],
       }),
       content: new FormControl(null, { validators: [Validators.required] }),
-      image: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType],
+      }),
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postID')) {
