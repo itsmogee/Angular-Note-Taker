155fbde67bcef01211f595b905bf3bfcfec1ae65
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-08-03 15:53:07 +0200

    added : middleware to protect routes
    created middleware function to protect routes that should be user authenticated

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const Post = require("../models/post");
 
 const router = express.Router();
+const checkAuth = require("../middleware/check-auth");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -28,6 +29,7 @@ const storage = multer.diskStorage({
 
 router.post(
   "",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     const url = `${req.protocol}://${req.get("host")}`;
@@ -52,6 +54,7 @@ router.post(
 
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -104,7 +107,7 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     res.status(200).json({ message: "Post deleted" });
   });
