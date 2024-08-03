1fbd14e9a1d2f791e47a7b8520ddbefd051832cc
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-08-03 18:17:44 +0200

    added : token functionality after logging in
    when loggin in succesfully a token will be used to autheticate the user when they
    perform certain actions i.e deleting/adding etc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { authInterceptor } from './auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
