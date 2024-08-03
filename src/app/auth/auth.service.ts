68c2b5e505701782e054567a61c4e4a41af4cae5
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-08-03 18:16:50 +0200

    updated : auth service added token
    added private token field with a get method to retrieve the token
    when logging in a token will be generated then stored in the service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,13 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -18,9 +24,10 @@ export class AuthService {
   loginUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
-      .post('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
       .subscribe((response) => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
